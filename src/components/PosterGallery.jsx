e67d221751e9e439c9f03e147e92e5079fc77048
Saif <saifeldeenadel77@gmail.com>
2026-02-11 16:43:17 +0200

    Added background image, animated posters, modal. TODO: request button? add poster images

--- src/components/PosterGallery.jsx
+++ src/components/PosterGallery.jsx
@@ -1,21 +1,60 @@
 import { useState } from "react";
 import PosterItem from "./PosterItem";
-
+import { motion, AnimatePresence } from "motion/react";
 
 export default function PosterGallery({ posters }) {
 	const [selectedPoster, setSelectedPoster] = useState(null);
 
 	return (
-		<div className="max-w-6xl mx-auto px-4">
-			<div className="grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-6">
+		<div className="max-w-6xl mx-auto px-5 py-20 sm:py-40">
+			<div className="grid grid-cols-2 sm:grid-cols-3 gap-y-8 gap-x-8 sm:gap-y-10 sm:gap-x-10">
 				{posters.map((poster) => (
-					<PosterItem
+					<motion.div
 						key={poster.id}
-						poster={poster}
-						onClick={() => setSelectedPoster(poster)}
-					/>
+						initial={{ opacity: 0, y: 20 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.8 }}
+						viewport={{ amount: 0.1 }}
+					>
+						<PosterItem
+							key={poster.id}
+							poster={poster}
+							onClick={() => setSelectedPoster(poster)}
+						/>
+					</motion.div>
 				))}
 			</div>
+
+			<AnimatePresence>
+				{selectedPoster && (
+					<>
+						{/* Dark Overlay */}
+						<motion.div
+							className="fixed inset-0 bg-black/70 z-40"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							onClick={() => setSelectedPoster(null)}
+						/>
+
+						{/* Centered Image */}
+						<div className="fixed inset-0 flex items-center justify-center z-50 pointer-events-none">
+							<div className="flex flex-col items-center gap-y-5 pointer-events-none">
+								<motion.img
+									src={selectedPoster.src.src}
+									width={selectedPoster.src.width}
+									height={selectedPoster.src.height}
+									layoutId={`poster-${selectedPoster.id}`}
+									className="max-w-[90vw] max-h-[90vh] shadow-2xl pointer-events-none"
+								/>
+								<div className="w-[50%] p-3 bg-background font">
+									Request it
+								</div>
+							</div>
+						</div>
+					</>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 }
--- src/components/PosterItem.jsx
+++ src/components/PosterItem.jsx
@@ -1,20 +1,20 @@
+import { motion } from "motion/react";
 export default function PosterItem({ poster, onClick }) {
-    console.log("PosterItem render:", poster);
 	return (
 		<div
 			key={poster.id}
-			className="cursor-pointer overflow-hidden shadow hover:shadow-lg transition"
+			className="cursor-pointer overflow-hidden shadow hover:shadow-xl transition"
 			onClick={onClick}
 		>
-			<img
+			<motion.img
 				src={poster.src.src}
 				width={poster.src.width}
 				height={poster.src.height}
 				alt={poster.title}
-				className="w-full h-auto object-cover"
+				layoutId={`poster-${poster.id}`}
+				className="w-full h-auto object-cover pointer-events-none"
 				loading="lazy"
 			/>
-			<div className="p-2 text-center font-semibold">{poster.title}</div>
 		</div>
 	);
 }
